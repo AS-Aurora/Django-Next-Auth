ed6fab5e3552e179f17fce96002e93aed8bced47
AS-Aurora <aayushsharma.lucky97@gmail.com>
2025-07-09 19:53:40 +0530

    Added the adapter.py to customize the emails and connect the verify email backend to the frontend page

---
+++ backend/backend/users/adapter.py
@@ -0,0 +1,24 @@
+from allauth.account.adapter import DefaultAccountAdapter
+from django.conf import settings
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+class CustomAccountAdapter(DefaultAccountAdapter):
+    def send_confirmation_mail(self, request, emailconfirmation, signup):
+        user = emailconfirmation.email_address.user
+        activate_url = f"{settings.FRONTEND_URL}/verify-email?key={emailconfirmation.key}"
+
+        ctx = {
+            "user": user,
+            "activate_url": activate_url,
+            "key": emailconfirmation.key,
+        }
+
+        # Render and debug
+        message = render_to_string("account/email/email_confirmation_message.txt", ctx)
+        subject = render_to_string("account/email/email_confirmation_subject.txt", ctx).strip()
+        print("Rendered message:\n", message)  # Debug print
+
+        email = EmailMessage(subject, message, to=[user.email])
+        email.send()
